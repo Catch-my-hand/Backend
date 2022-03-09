c883547d1ec772ac61e1656e1da53aa5df9d64a3
JoeCP17 <at365@naver.com>
2022-03-09 16:34:37 +0900

    Spring-boot 추가 Dockerfile 변경 예정

--- backend/src/main/java/com/catch_my_hand/backend/BackendApplication.java
+++ backend/src/main/java/com/catch_my_hand/backend/BackendApplication.java
@@ -2,9 +2,18 @@ package com.catch_my_hand.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BackendApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Welcome Catchmyhand Backend API";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
