586c006def688eb9ddbdbcfd7e28a9b417dc7379
Joe <at365@naver.com>
2022-04-06 13:19:18 +0900

    Center수정 + User 회원가입 구현 완료

---
+++ backend/src/main/java/com/catch_my_hand/backend/Center_data/Entity/Center.java
@@ -0,0 +1,38 @@
+package com.catch_my_hand.backend.Center_data.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Center {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id ;
+
+    private String desertionNo; //유기번호
+    private String processState; //상태
+    private String noticeSdt; //공고일
+    private String noticeEdt; //공고종료일
+    private String kindCd; //품종
+    private String colorCd; //색깔
+    private String age; //나이
+    private String weight; //무게
+    private String sexCd; //성별
+    private String neuterYn; //중성화 여부
+    private String specialMark; //특징
+    private String careNm; //보호소 명
+    private String careAddr; //보호 장소
+    private String orgNm; //관할 기관
+    private String popfile; //이미지 파일
+    private String filename; //썸네일 이미지 파일
+
+
+    }
+
