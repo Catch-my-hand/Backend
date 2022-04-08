459cf3c0421e71db52e57280fac9b558a3d43443
sugang258 <olivia258@naver.com>
2022-04-08 18:42:38 +0900

    스프링 어노테이션 활용해서 코드 고침

--- backend/src/main/java/com/catch_my_hand/backend/Center_data/CenterController.java
+++ backend/src/main/java/com/catch_my_hand/backend/Center_data/CenterController.java
@@ -1,21 +1,21 @@
 package com.catch_my_hand.backend.Center_data;
 
-import org.json.XML;
-import org.json.simple.parser.JSONParser;
+import com.catch_my_hand.backend.Center_data.Entity.Center;
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
-import java.io.BufferedReader;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -23,107 +23,73 @@ public class CenterController {
 
 
     @GetMapping("/center")
-    public List<Object> callApi() throws IOException {
+    public String callApi() throws IOException {
 
 
-        StringBuilder result = new StringBuilder();
-        String jsonPrintString = null;
         List<Object> list = null;
+        list = new ArrayList<>();
+        Center center = null;
+
+        //공공데이터 포털 API에서 유기동물 정보 가지고 옴.
         try {
-            String ulrStr = "http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic" +
-                    "?" + "bgnde" + "=" + "20140301" + "&" + "endde" + "=" + "20211231" + "&" + "pageNo" + "=" + "1" + "&" + "numOfRows" + "=" + "100" +
-                    "&" + "ServiceKey" + "=" + "%2BRTldbf2L3IqeIcRuy2XJ6N6j3fVTPIw5lakLVIhVFppiIZnC60PAY08Du5ERM5fFkYyN%2BHmMU7lUGKD5zOIQA%3D%3D";
+            StringBuilder urlStr = new StringBuilder("http://openapi.animal.go.kr/openapi/service/rest/abandonmentPublicSrvc/abandonmentPublic");
+            urlStr.append("?" + URLEncoder.encode("bgnde", "UTF-8") + "=20211201"); //bgnde : 유기날짜 (검색 시작일) (YYYYMMDD)
+            urlStr.append("&" + URLEncoder.encode("endde", "UTF-8") + "=20220401"); //endde : 유기날짜 (검색 종료일) (YYYYMMDD)
+            urlStr.append("&" + URLEncoder.encode("upkind", "UTF-8") + "=417000"); //upkind : 품종코드 (개:417000)
+            urlStr.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=1");      //pageNo : 페이지 번호
+            urlStr.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=20");  //numOfRows : 페이지당 보여줄 개수
+            urlStr.append("&" + URLEncoder.encode("ServiceKey", "UTF-8") + "=%2BRTldbf2L3IqeIcRuy2XJ6N6j3fVTPIw5lakLVIhVFppiIZnC60PAY08Du5ERM5fFkYyN%2BHmMU7lUGKD5zOIQA%3D%3D"); //서비스 키
 
+            String url = urlStr.toString();
 
-            URL url = new URL(ulrStr);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = (Document) dBuilder.parse(url);
+            NodeList nodelist = doc.getElementsByTagName("item");
 
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
+            for (int temp = 0; temp < nodelist.getLength(); temp++) {
+                Node nNode = nodelist.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) nNode;
 
-            BufferedReader br;
+                    center = new Center();
+                    center.setDesertionNo(getTagValue("desertionNo", element));
+                    center.setHappenDt(getTagValue("happenDt", element));
+                    center.setProcessState(getTagValue("processState", element));
+                    center.setNoticeSdt(getTagValue("noticeSdt", element));
+                    center.setNoticeEdt(getTagValue("noticeEdt", element));
+                    center.setKindCd(getTagValue("kindCd", element));
+                    center.setColorCd(getTagValue("colorCd", element));
+                    center.setAge(getTagValue("age", element));
+                    center.setWeight(getTagValue("weight", element));
+                    center.setSexCd(getTagValue("sexCd", element));
+                    center.setNeuterYn(getTagValue("neuterYn", element));
+                    center.setSpecialMark(getTagValue("specialMark", element));
+                    center.setCareNm(getTagValue("careNm", element));
+                    center.setCareAddr(getTagValue("CareAddr", element));
+                    center.setOrgNm(getTagValue("orgNm", element));
+                    center.setPopfile(getTagValue("popfile", element));
+                    center.setFilename(getTagValue("filename", element));
 
-            br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
-
-            String returnLine;
-
-            while ((returnLine = br.readLine()) != null) {
-                result.append(returnLine + "\n\r");
+                    list.add(center);
+                }
             }
-
-            org.json.JSONObject jsonObject = XML.toJSONObject(result.toString());
-            jsonPrintString = jsonObject.toString();
-
-
-            org.json.simple.JSONObject jObj = null;
-
-            JSONParser jsonParser = new JSONParser();
-
-            org.json.simple.JSONObject jsonObj = (org.json.simple.JSONObject) jsonParser.parse(jsonPrintString);
-
-            org.json.simple.JSONObject parseResponse = (org.json.simple.JSONObject) jsonObj.get("response");
-
-            org.json.simple.JSONObject parseBody = (org.json.simple.JSONObject) parseResponse.get("body");
-
-            org.json.simple.JSONObject parseItems = (org.json.simple.JSONObject) parseBody.get("items");
-            org.json.simple.JSONArray array = (org.json.simple.JSONArray) parseItems.get("item");
-
-            list = new ArrayList<Object>();
-
-
-
-            for (int i = 0; i < array.size(); i++) {
-                jObj = (org.json.simple.JSONObject) array.get(i);
-
-                String desertionNo = String.valueOf(jObj.get("desertionNo")); //유기번호
-                String happenDt = String.valueOf(jObj.get("happenDt")); //접수일
-                String processState = String.valueOf(jObj.get("processState")); //상태
-                String noticeSdt = String.valueOf(jObj.get("noticeSdt")); //공고일
-                String noticeEdt = String.valueOf(jObj.get("noticeEdt")); //공고종료일
-                String kindCd = String.valueOf(jObj.get("kindCd")); //품종
-                String colorCd = String.valueOf(jObj.get("colorCd")); //색깔
-                String age = String.valueOf(jObj.get("age")); //나이
-                String weight = String.valueOf(jObj.get("weight")); //무게
-                String sexCd = String.valueOf(jObj.get("sexCd")); // 성별
-                String neuterYn = String.valueOf(jObj.get("neuterYn")); //중성화 여부
-                String specialMark = String.valueOf(jObj.get("specialMark")); //특징
-                String careNm = String.valueOf(jObj.get("careNm")); //보호소 명
-                String careAddr = String.valueOf(jObj.get("careAddr")); //보호 장소
-                String orgNm = String.valueOf(jObj.get("orgNm")); //관할기관
-                String popfile = String.valueOf(jObj.get("popfile")); //이미지 파일
-                String filename = String.valueOf(jObj.get("filename")); //썸네일 이미지 파일
-
-                list.add(center1(desertionNo, happenDt, processState, noticeSdt, noticeEdt, kindCd, colorCd, age, weight, sexCd, neuterYn,
-                        specialMark, careNm,careAddr, orgNm, popfile, filename));
-
-            }
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return list;
+        String listjson = new Gson().toJson(list);
+        return listjson;
+    }
+    
+    private static String getTagValue(String tag, Element ele) {
+        try {
+            NodeList nodeList = ele.getElementsByTagName(tag).item(0).getChildNodes();
+            Node nValue = (Node) nodeList.item(0);
+            if(nValue == null) {
+                return null;
+            }
+            return nValue.getNodeValue();
+        } catch (Exception e) {return null;}
     }
 
-    private Object center1(@RequestParam String desertionNo, @RequestParam String happenDt, @RequestParam String processState, @RequestParam String noticeSdt, @RequestParam String noticeEdt, @RequestParam String kindCd, @RequestParam String colorCd, @RequestParam String age, @RequestParam String weight, @RequestParam String sexCd, @RequestParam String neuterYn, @RequestParam String specialMark, @RequestParam String careNm, @RequestParam String careAddr, @RequestParam String orgNm, @RequestParam String popfile, @RequestParam String filename) {
-        Map<String, Object> rmap = new HashMap<>();
-        rmap.put("desertionNo",desertionNo);
-        rmap.put("happenDt", happenDt);
-        rmap.put("processState", processState);
-        rmap.put("noticeSdt", noticeSdt);
-        rmap.put("noticeEdt", noticeEdt);
-        rmap.put("kindCd", kindCd);
-        rmap.put("colorCd", colorCd);
-        rmap.put("age", age);
-        rmap.put("weight", weight);
-        rmap.put("sexCd", sexCd);
-        rmap.put("neuterYn", neuterYn);
-        rmap.put("specialMark", specialMark);
-        rmap.put("careNm", careNm);
-        rmap.put("careAddr", careAddr);
-        rmap.put("orgNm", orgNm);
-        rmap.put("popfile", popfile);
-        rmap.put("filename", filename);
-
-        return rmap;
-    }
 }
--- backend/src/main/java/com/catch_my_hand/backend/Center_data/Entity/Center.java
+++ backend/src/main/java/com/catch_my_hand/backend/Center_data/Entity/Center.java
@@ -17,6 +17,7 @@ public class Center {
     private Integer id ;
 
     private String desertionNo; //유기번호
+    private String happenDt; //접수일
     private String processState; //상태
     private String noticeSdt; //공고일
     private String noticeEdt; //공고종료일
@@ -31,8 +32,84 @@ public class Center {
     private String careAddr; //보호 장소
     private String orgNm; //관할 기관
     private String popfile; //이미지 파일
-    private String filename; //썸네일 이미지 파일
+    private String filename; //썸네일이미지 파일
 
+//GETTER, SETTER
+    public String getDesertionNo() { return desertionNo; }
 
+    public void setDesertionNo(String desertionNo) {this.desertionNo = desertionNo;}
+    
+    public String getHappenDt() {return happenDt;}
+    
+    public void setHappenDt(String happenDt) {this.happenDt = happenDt;}
+   
+    public String getProcessState() {return processState;}
+
+    public void setProcessState(String processState) {this.processState = processState;}
+
+    public String getNoticeSdt() { return noticeSdt; }
+
+    public void setNoticeSdt(String noticeSdt) {this.noticeSdt = noticeSdt;}
+
+    public String getNoticeEdt() { return noticeEdt; }
+
+    public void setNoticeEdt(String noticeEdt) {this.noticeEdt = noticeEdt;}
+
+    public String getKindCd() {return kindCd;}
+
+    public void setKindCd(String kindCd) {this.kindCd = kindCd; }
+
+    public String getColorCd() {return colorCd; }
+
+    public void setColorCd(String colorCd) { this.colorCd = colorCd;}
+
+    public String getAge() {return age;}
+
+    public void setAge(String age) {this.age = age;}
+
+    public String getWeight() {return weight;}
+
+    public void setWeight(String weight) {this.weight = weight;}
+
+    public String getSexCd() {return sexCd;}
+
+    public void setSexCd(String sexCd) {this.sexCd = sexCd;}
+
+    public String getNeuterYn() {return neuterYn;}
+
+    public void setNeuterYn(String neuterYn) {this.neuterYn = neuterYn;}
+
+    public String getSpecialMark() { return specialMark; }
+
+    public void setSpecialMark(String specialMark) {this.specialMark = specialMark; }
+
+    public String getCareNm() {return careNm;}
+
+    public void setCareNm(String careNm) {this.careNm = careNm; }
+
+    public String getCareAddr() {return careAddr;}
+
+    public void setCareAddr(String careAddr) {this.careAddr = careAddr;}
+
+    public String getOrgNm() {return orgNm;}
+
+    public void setOrgNm(String orgNm) {this.orgNm = orgNm;}
+
+    public String getPopfile() {return popfile; }
+
+    public void setPopfile(String popfile) {this.popfile = popfile;}
+
+    public String getFilename() {return filename; }
+
+    public void setFilename(String filename) {this.filename = filename; }
+
+    @Override
+    public String toString() {
+        return "Center [desertionNo =" + desertionNo + ", happenDt=" + happenDt + ", processState=" + processState + ", noticeSdt=" + noticeSdt + ", noticeEdt=" +noticeEdt +
+                ", kindCd=" + kindCd + ", colorCd=" + colorCd + ", age=" + age + ", weight=" +weight + ", sexCd=" + sexCd + ", neuterYn=" + neuterYn + ", specialMark=" + specialMark +
+                ", careNm=" + careNm + ", careAddr=" + careAddr + ", orgNm=" + orgNm + ", popfile=" + popfile + ", filename=" + filename + "]";
     }
+}
+
+    
 
