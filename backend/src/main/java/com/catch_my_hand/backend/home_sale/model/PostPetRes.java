dfb53140f6cf7a9c2bdf8783f9de338af8bd6292
Joe <at365@naver.com>
2022-04-08 21:54:13 +0900

    Pet post + center 수정

---
+++ backend/src/main/java/com/catch_my_hand/backend/home_sale/model/PostPetRes.java
@@ -0,0 +1,12 @@
+package com.catch_my_hand.backend.home_sale.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PatchPetRes {
+    private int productidx;
+}
---
+++ backend/src/main/java/com/catch_my_hand/backend/image/ImageProvider.java
@@ -0,0 +1,2 @@
+package com.catch_my_hand.backend.image;public class ImageProvider {
+}
