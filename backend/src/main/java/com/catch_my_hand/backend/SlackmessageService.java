406a43e46de5626dd23dfbf0a1f06d7d8690c192
Joe <at365@naver.com>
2022-05-25 23:55:13 +0900

    delete:url

--- backend/src/main/java/com/catch_my_hand/backend/SlackmessageService.java
+++ backend/src/main/java/com/catch_my_hand/backend/SlackmessageService.java
@@ -13,7 +13,7 @@ public class SlackmessageService {
             WebhookResponse response = null;
             Slack slack = Slack.getInstance();
 
-            String webhookUrl = "https://hooks.slack.com/services/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";
+            String webhookUrl = "webhookURL";
 
             Payload payload = Payload.builder().text(text).build();
             response = slack.send(webhookUrl, payload);
